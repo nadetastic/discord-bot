085957cca3afad4a2ed0f43d7b142ba48ff37e48
Dan Kiuna <dankiuna@gmail.com>
2024-01-03 13:52:02 -0600

    Update seed.ts

--- apps/discord-bot-frontend/scripts/seed.ts
+++ apps/discord-bot-frontend/scripts/seed.ts
@@ -10,6 +10,8 @@ function createFakeUsers(): Prisma.DiscordUserCreateInput[] {
   }))
 }
 
+/* test */
+
 async function getRandomFakeUser(): Promise<DiscordUser> {
   const [user] = (await prisma.$queryRawUnsafe(
     `SELECT * FROM "DiscordUser" ORDER BY RANDOM() LIMIT 1;`
